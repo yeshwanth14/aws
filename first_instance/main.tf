7dcdec9745fce8b88b06be5da6269bcef1e37722
yeshwanth14 <61215457+yeshwanth14@users.noreply.github.com>
2021-02-15 17:58:25 +0530

    Update main.tf

--- first_instance/main.tf
+++ first_instance/main.tf
@@ -1,57 +1,15 @@
-provider "google" {
- project        = "ethereal-terra-279604"
- credentials    = "${file("aws/credentials/secrets.json")}"
- region         = "us-central1-a"
+resource "google_compute_instance" "default" {
+name = "test"
+machine_type = var.machine_type
+zone = "us-central1-a"
+
+
+boot_disk {
+    initialize_params {
+        image = var.image 
  }
-resource "google_container_cluster" "cluster" {
-  name               = "demo-cluster"
-  location           = "us-central1-a"
-  network            = "default"
-  subnetwork         = "default"
-  master_auth {
-    username = "rapid"
-    password = "admin#jenkins#123"
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-  workload_identity_config {
-  identity_namespace = "${var.project}.svc.id.goog"
 }
-
-node_pool {
-    name       = "default-pool"
-    node_count = 2
-    
-    
-    management {
-    auto_repair  = true
-    auto_upgrade = true
-    
-  }
-
-
- 
- node_config {
-    preemptible  = true
-    machine_type = "n1-standard-2"
-    
-  
-
- 
-  oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-  shielded_instance_config {
-              
-    enable_secure_boot          = true
-    }
- 
-  }
- 
-  
+network_interface {
+    network = "default"
  }
 }
